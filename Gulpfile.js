a9218e134b89363489d7b6673af35f6c5be63bff
durantjay <durantjay1@gmail.com>
2016-02-23 16:07:26 -0800

    Add directories and dependencies to project repo

---
+++ Gulpfile.js
@@ -0,0 +1,70 @@
+
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer =require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var cssnano = require('gulp-cssnano');
+var eslint =require('gulp-eslint');
+var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var babelify = require('babelify');
+var watchify = require('watchify');
+var gutil = require('gulp-util');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+
+var b = browserify({
+		entries: './develop/app.jsx',
+		transform: [babelify],
+		plugin: [watchify],
+		extensions:['.jsx'],
+		cache:{},
+		packageCache: {},
+		debug:true
+	})
+
+
+function bundle(){
+	gutil.log('Compiling Javascript to ES5...');
+	return b.bundle()
+		   .on('error',gutil.log.bind(gutil,'Browserify Error'))
+		   .pipe(source('app.js'))
+		   .pipe(buffer())
+		   .pipe(sourcemaps.init({loadMaps:true}))
+		   .pipe(sourcemaps.write('./'))
+		   .pipe(gulp.dest('js/'))
+		   .pipe(browserSync.stream());
+}
+
+gulp.task('compilejs',bundle);
+b.on('update',bundle);
+b.on('log',gutil.log);
+
+
+
+gulp.task('styles',function(){
+	return gulp.src('sass/**/*.scss')
+		.pipe(sass().on('error',sass.logError))
+		.pipe(autoprefixer())
+		.pipe(gulp.dest('css/'))
+		.pipe(reload({stream:true}))
+		.pipe(rename({suffix:'.min'}))
+		.pipe(sourcemaps.init())
+		.pipe(cssnano())
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest('css/'))
+})
+
+gulp.task('default',['styles'],function(){
+	browserSync.init({
+		server:'./'
+	})
+	gulp.watch('sass/**/*.scss',["styles"]);
+	gulp.watch('*.html').on('change',reload);
+})
+
+
